7846a6a38762d9c11aa8a9386331109d569e40ce
tahmid-saj <tahmid.saj@gmail.com>
2024-09-22 14:50:23 -0400

    monitor tests

---
+++ monitor/ping_test.go
@@ -0,0 +1,35 @@
+package monitor
+
+import (
+	"context"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	context := context.Background()
+	tests := []struct {
+		URL string
+		Up bool
+	} {
+		{ "google.com", true },
+		{ "youtube.com", true },
+		// Test both with and without "https://"
+		{"httpbin.org/status/200", true},
+		{"https://httpbin.org/status/200", true},
+
+		// 4xx and 5xx should considered down.
+		{"httpbin.org/status/400", false},
+		{"https://httpbin.org/status/500", false},
+		// Invalid URLs should be considered down.
+		{"invalid://scheme", false},
+	}
+
+	for _, test := range tests {
+		res, err := Ping(context, test.URL)
+		if err != nil {
+			t.Errorf("url %s: unexpected error occurred: %v", test.URL, err)
+		} else if res.Up != test.Up {
+			t.Errorf("url %s: got up=%v, want %v", test.URL, res.Up, test.Up)
+		}
+	}
+}
